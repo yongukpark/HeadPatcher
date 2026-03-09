83fec49ac8a273f022bb2ab3f6cbcef326109f90
imjiyu <127571827+imjiyu@users.noreply.github.com>
2026-03-09 14:36:01 +0900

    Create color_fs_corr.py

---
+++ datasets/by_category/colors/color_fs_corr.py
@@ -0,0 +1,75 @@
+{"prompt": "The color of banana is yellow.\nThe color of snow is '"}
+{"prompt": "The color of banana is yellow.\nThe color of grass is '"}
+{"prompt": "The color of banana is yellow.\nThe color of strawberry is '"}
+{"prompt": "The color of banana is yellow.\nThe color of charcoal is '"}
+{"prompt": "The color of banana is yellow.\nThe color of banana is '"}
+{"prompt": "The color of banana is yellow.\nThe color of sun is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cucumber is '"}
+{"prompt": "The color of banana is yellow.\nThe color of sea is '"}
+{"prompt": "The color of banana is yellow.\nThe color of almond is '"}
+{"prompt": "The color of banana is yellow.\nThe color of fire truck is '"}
+{"prompt": "The color of banana is yellow.\nThe color of orange is '"}
+{"prompt": "The color of banana is yellow.\nThe color of seeweed is '"}
+{"prompt": "The color of banana is yellow.\nThe color of tomato is '"}
+{"prompt": "The color of banana is yellow.\nThe color of blueberry is '"}
+{"prompt": "The color of banana is yellow.\nThe color of clover is '"}
+{"prompt": "The color of banana is yellow.\nThe color of dog is '"}
+{"prompt": "The color of banana is yellow.\nThe color of moon is '"}
+{"prompt": "The color of banana is yellow.\nThe color of stop sign is '"}
+{"prompt": "The color of banana is yellow.\nThe color of spinach is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cake is '"}
+{"prompt": "The color of banana is yellow.\nThe color of dolphin is '"}
+{"prompt": "The color of banana is yellow.\nThe color of lettuce is '"}
+{"prompt": "The color of banana is yellow.\nThe color of chalk is '"}
+{"prompt": "The color of banana is yellow.\nThe color of frog is '"}
+{"prompt": "The color of banana is yellow.\nThe color of chili is '"}
+{"prompt": "The color of banana is yellow.\nThe color of honey is '"}
+{"prompt": "The color of banana is yellow.\nThe color of sunflower is '"}
+{"prompt": "The color of banana is yellow.\nThe color of brownie is '"}
+{"prompt": "The color of banana is yellow.\nThe color of teeth is '"}
+{"prompt": "The color of banana is yellow.\nThe color of bamboo is '"}
+{"prompt": "The color of banana is yellow.\nThe color of sand is '"}
+{"prompt": "The color of banana is yellow.\nThe color of blood is '"}
+{"prompt": "The color of banana is yellow.\nThe color of yogurt is '"}
+{"prompt": "The color of banana is yellow.\nThe color of egg yolk is '"}
+{"prompt": "The color of banana is yellow.\nThe color of turtle is '"}
+{"prompt": "The color of banana is yellow.\nThe color of tongue is '"}
+{"prompt": "The color of banana is yellow.\nThe color of milk is '"}
+{"prompt": "The color of banana is yellow.\nThe color of tree is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cherry is '"}
+{"prompt": "The color of banana is yellow.\nThe color of honeybees is '"}
+{"prompt": "The color of banana is yellow.\nThe color of whiteboard is '"}
+{"prompt": "The color of banana is yellow.\nThe color of lime is '"}
+{"prompt": "The color of banana is yellow.\nThe color of deer is '"}
+{"prompt": "The color of banana is yellow.\nThe color of navy uniform is '"}
+{"prompt": "The color of banana is yellow.\nThe color of soot is '"}
+{"prompt": "The color of banana is yellow.\nThe color of kiwi is '"}
+{"prompt": "The color of banana is yellow.\nThe color of dragonfruit is '"}
+{"prompt": "The color of banana is yellow.\nThe color of egg shell is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cabbage is '"}
+{"prompt": "The color of banana is yellow.\nThe color of spaghetti is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cloud is '"}
+{"prompt": "The color of banana is yellow.\nThe color of aloe is '"}
+{"prompt": "The color of banana is yellow.\nThe color of sunglass is '"}
+{"prompt": "The color of banana is yellow.\nThe color of ruby is '"}
+{"prompt": "The color of banana is yellow.\nThe color of ocean is '"}
+{"prompt": "The color of banana is yellow.\nThe color of avocado is '"}
+{"prompt": "The color of banana is yellow.\nThe color of clear sky is '"}
+{"prompt": "The color of banana is yellow.\nThe color of butterfly is '"}
+{"prompt": "The color of banana is yellow.\nThe color of bluebells is '"}
+{"prompt": "The color of banana is yellow.\nThe color of fire is '"}
+{"prompt": "The color of banana is yellow.\nThe color of forest is '"}
+{"prompt": "The color of banana is yellow.\nThe color of coffee is '"}
+{"prompt": "The color of banana is yellow.\nThe color of apple is '"}
+{"prompt": "The color of banana is yellow.\nThe color of emerald is '"}
+{"prompt": "The color of banana is yellow.\nThe color of ketchup is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cactus is '"}
+{"prompt": "The color of banana is yellow.\nThe color of popcorn is '"}
+{"prompt": "The color of banana is yellow.\nThe color of tangerine is '"}
+{"prompt": "The color of banana is yellow.\nThe color of peanut is '"}
+{"prompt": "The color of banana is yellow.\nThe color of broccoli is '"}
+{"prompt": "The color of banana is yellow.\nThe color of rose is '"}
+{"prompt": "The color of banana is yellow.\nThe color of pumpkin is '"}
+{"prompt": "The color of banana is yellow.\nThe color of shamrock is '"}
+{"prompt": "The color of banana is yellow.\nThe color of water is '"}
+{"prompt": "The color of banana is yellow.\nThe color of cherry blossom is '"}
